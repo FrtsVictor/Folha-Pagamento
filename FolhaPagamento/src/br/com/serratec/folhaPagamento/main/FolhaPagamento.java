6d46a9ba5a9fd93dac4a91626b46cbebd269ac0d
FrtsVictor <victorfreitas.job@gmail.com>
2020-08-15 19:55:30 -0300

    Removendo a criacao do metodo para exibição do Menu no corpo da classe Folha Pagamento

--- FolhaPagamento/src/br/com/serratec/folhaPagamento/main/FolhaPagamento.java
+++ FolhaPagamento/src/br/com/serratec/folhaPagamento/main/FolhaPagamento.java
@@ -6,41 +6,31 @@ import br.com.serratec.folhaPagamento.classeConcreta.Menu;
 
 public class FolhaPagamento {
 
-	public static void exibirMenu() {
-		System.out.println("_______Sistema para Calculo de Folha de Pagamento_______\n");
-		System.out.println("1 - Para adicionar funcionarios e dependentes via console");
-		System.out.println("2 - Para listar funcionarios e dependentes adicionados via console");
-		System.out.println("3 - Para gravar entrada de funcionarios via console.");
-		System.out.println("4 - Para adicionar e calcular funcionarios e dependentes via arquivo .csv");
-		System.out.println("0 - Para sair do menu");
-	}
-
 	public static void main(String[] args) {
 			
 		
-		Menu scannersCases = new Menu();
+		Menu menuC = new Menu();
 		
 		System.out.println("Bem vindo ao sistema de Calculo IR para funcionarios!!!\n");
 		
 		Scanner inMenu = new Scanner(System.in);
 		int menus;
 		do {
-			exibirMenu();
+			menuC.exibirMenu();
 			menus = inMenu.nextInt();
 			
 			switch (menus) {
 			case 1: {
-				scannersCases.adicionarPessoasConsole(); 	
+				menuC.adicionarPessoasConsole(); 	
 			}break;
 			case 2:{
-				scannersCases.listarPessoasConsole();
+				menuC.listarPessoasConsole();
 			}break;
 			case 3:
-				scannersCases.escreverSaida();
+				menuC.escreverSaida();
 			break;
 			case 4:
-				System.out.println("Opcao 4 selecionada.");
-				scannersCases.calcularViaCSV();
+				menuC.calcularViaCSV();
 				break;
 			case 0:
 				System.out.println("Sistema encerrado.");
@@ -50,7 +40,7 @@ public class FolhaPagamento {
 			
 		}while(menus!=0);
 	inMenu.close();
-	scannersCases.fecharScanners();
+	menuC.fecharScanners();
 
 }
 
